851710878cf433a75553b16e2e42876e1d341a72
poojavaja84 <pooja.vaja@gmail.com>
2021-07-22 10:32:41 -0700

    Add project files.

---
+++ OpenEMRApplication/LoginTest.cs
@@ -0,0 +1,43 @@
+﻿using NUnit.Framework;
+using OpenQA.Selenium;
+using OpenQA.Selenium.Chrome;
+using OpenQA.Selenium.Support.UI;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using WebDriverManager;
+using WebDriverManager.DriverConfigs.Impl;
+
+namespace OpenEMRApplication
+{
+    class LoginTest
+    {
+        [Test]
+        public void ValidCredentialTest()
+        {
+            new DriverManager().SetUpDriver(new ChromeConfig());
+            IWebDriver driver = new ChromeDriver();
+            driver.Manage().Window.Maximize();
+            driver.Manage().Timeouts().ImplicitWait = TimeSpan.FromSeconds(30);
+            driver.Url = "http://demo.openemr.io/b/openemr/interface/login/login.php?site=default";
+
+            driver.FindElement(By.CssSelector("#authUser123")).SendKeys("admin");
+            driver.FindElement(By.Id("clearPass")).SendKeys("pass");
+            SelectElement select = new SelectElement(driver.FindElement(By.Name("languageChoice")));
+            select.SelectByText("English (Indian)");
+
+            driver.FindElement(By.XPath("//button[@type='submit']")).Click();
+
+            WebDriverWait wait = new WebDriverWait(driver, TimeSpan.FromSeconds(50));
+            wait.Until(x => x.FindElement(By.XPath("//div[contains(text(),'Patient/Client')]")));
+
+            string actualValue = driver.Title;
+
+
+
+            Assert.AreEqual("OpenEMR123", actualValue);
+
+
+        }
+    }
+}
